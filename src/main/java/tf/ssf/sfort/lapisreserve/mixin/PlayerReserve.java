585bfeebf679d85aae9fe8c90eaad53ad9de07d3
Ves <vesmaybevesper@gmail.com>
2025-04-11 14:48:46 -0400

    Build for 1.21.5

--- src/main/java/tf/ssf/sfort/lapisreserve/mixin/PlayerReserve.java
+++ src/main/java/tf/ssf/sfort/lapisreserve/mixin/PlayerReserve.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import tf.ssf.sfort.lapisreserve.PlayerInterface;
+
+import java.util.Objects;
 
 @Mixin(PlayerInventory.class)
 public class PlayerReserve implements PlayerInterface {
@@ -26,14 +29,13 @@ public class PlayerReserve implements PlayerInterface {
 		if (lapisreserve.isEmpty()) return;
 		NbtCompound compoundTag = new NbtCompound();
 		compoundTag.putByte("LapisReserve", (byte)0);
-		tag.add(lapisreserve.encode(this.player.getRegistryManager(), compoundTag));
+		tag.add(lapisreserve.toNbt(this.player.getRegistryManager(), compoundTag));
 	}
 	@Inject(method = "readNbt(Lnet/minecraft/nbt/NbtList;)V",at=@At("HEAD"))
 	public void deserialize(NbtList tag, CallbackInfo info) {
 		for(int i = 0; i < tag.size(); ++i) {
-			NbtCompound compoundTag = tag.getCompound(i);
-			if (compoundTag.contains("LapisReserve")){
-				lapisreserve = ItemStack.fromNbt(this.player.getRegistryManager(), compoundTag).orElse(ItemStack.EMPTY);
+            if (Objects.equals(((NbtElement) tag).toString(), "LapisReserve")){
+				lapisreserve = ItemStack.fromNbt(this.player.getRegistryManager(), tag).orElse(ItemStack.EMPTY);
 				tag.remove(i);
 				break;
 			}
